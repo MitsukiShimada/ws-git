b9251cc366fa4862191176278de50ae4bcae0aa1
島田光基 <mitsuki.shimada.1007@gmail.com>
2016-10-18 16:29:23 +0900

    レンタルサーバーにDB移植した

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ var wss = new WebSocketServer({server: server});
 //console.log("コンソール：http server listening on %d", port);
 //console.log("コンソール：websocket server created");
 //島田追加
-var mysql = require(['node_modules/mysql']);	//require.jsを使用する場合
-// var mysql = require('mysql');	//browserifyを使用する場合
+// var mysql = require(['node_modules/mysql']);	//require.jsを使用する場合
+var mysql = require('mysql');	//browserifyを使用する場合
 
 var db_connection;	//mysqlの接続
 //接続設定の用意
