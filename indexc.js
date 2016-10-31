773999ac9d9165c0985cf777c25edd141a5218bd
島田光基 <mitsuki.shimada.1007@gmail.com>
2016-10-31 16:30:57 +0900

    dbからとってきたデータを変換するメソッドを実装中

--- indexc.js
+++ indexc.js
@@ -291,6 +291,7 @@ ws.onmessage = function (event) {
 		}
 	}else if(messages.function == "readLinesScriptDataByScene"){
 		for(key in messages.text){
+			console.log("messages.test[0].line= " + messages.text([0].line));
 			console.log(messages.text[key].line);
 			chat_fld.innerHTML += "function: " + messages.function + " data: " + messages.text[key].line + "<br>";
 			console.log(convertStringDataInto1DStringArray(messages.text[key].line));
